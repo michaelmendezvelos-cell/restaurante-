16acb25e7bb2ec940cf321b4f2eba857e377b222
michaelmendezvelos-cell <michaelmendezvelos@gmail.com>
2026-05-14 23:28:15 -0400

    Change home route message for restaurant info
    Updated home route response to include restaurant details.

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "HEURY OK 🚀"
+    return "restaurante heruy"
+
+"Menu del restaurante:hamburguesas 100$, pizza:200$, mofongo y mas ha 150$ en adelante"
+
+"El Restaurante Heury es un lugar creado para ofrecer comida rápida, deliciosa y a buen precio. Nos especializamos en hamburguesas, pizzas y bebidas refrescantes, preparadas con ingredientes frescos y de calidad"
+
+"llamanos al 809-000-000"
+
+"TE ESPERAMOS"
 
 app.run(host="0.0.0.0", port=10000)
